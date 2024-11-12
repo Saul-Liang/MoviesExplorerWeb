a60373d958cb8a67a657fce0b872c5fdc9baadaf
Saul-Liang <liang.saul@gmail.com>
2024-11-12 00:07:45 +0000

    add movie model

---
+++ apps/web/src/shared/models/movie.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export type Movie = z.infer<typeof movieSchema>;
+
+export const movieSchema = z.object({
+  adult: z.boolean(),
+  backdrop_path: z.string(),
+  genre_ids: z.array(z.number()),
+  id: z.number(),
+  original_language: z.string(),
+  original_title: z.string(),
+  overview: z.string(),
+  popularity: z.number(),
+  poster_path: z.string(),
+  release_date: z.string(),
+  title: z.string(),
+  vote_average: z.number(),
+  vote_count: z.number(),
+});
