978812b43bcc4a3f4ab733f25c7a3e785b30030c
Saul-Liang <liang.saul@gmail.com>
2024-11-13 15:36:47 +0000

    update text component

--- packages/ui/src/components/ui/text.tsx
+++ packages/ui/src/components/ui/text.tsx
@@ -2,33 +2,26 @@ import { cn } from "@ui/lib/utils";
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
-const textVariants = cva("", {
-  variants: {
-    size: {
-      sm: "text-sm [&:not(:first-child)]:mt-3",
-      md: "text-md [&:not(:first-child)]:mt-6",
-      lg: "text-lg [&:not(:first-child)]:mt-9",
-    },
-  },
-  defaultVariants: {
-    size: "md",
-  },
-});
+// const textVariants = cva("", {
+//   variants: {
+//     size: {
+//       sm: "text-sm [&:not(:first-child)]:mt-3",
+//       md: "text-md [&:not(:first-child)]:mt-6",
+//       lg: "text-lg [&:not(:first-child)]:mt-9",
+//     },
+//   },
+//   defaultVariants: {
+//     size: "md",
+//   },
+// });
 
-export type TextProps = React.HTMLAttributes<HTMLParagraphElement> &
-  VariantProps<typeof textVariants>;
+export type TextProps = React.HTMLAttributes<HTMLParagraphElement>;
 
 const Text = React.forwardRef<HTMLParagraphElement, TextProps>(
-  ({ className, size, ...props }, ref) => {
-    return (
-      <p
-        className={cn(textVariants({ size }), className)}
-        ref={ref}
-        {...props}
-      />
-    );
+  ({ className, ...props }, ref) => {
+    return <p className={className} ref={ref} {...props} />;
   },
 );
 Text.displayName = "Text";
 
-export { Text, textVariants };
+export { Text };
