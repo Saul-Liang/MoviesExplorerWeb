8c74039f0f10a069074663c34068db107194c8fa
Saul-Liang <liang.saul@gmail.com>
2024-11-13 22:08:06 +0000

    remove comments from text component

--- packages/ui/src/components/ui/text.tsx
+++ packages/ui/src/components/ui/text.tsx
@@ -1,19 +1,4 @@
-import { cn } from "@ui/lib/utils";
 import * as React from "react";
-import { cva, type VariantProps } from "class-variance-authority";
-
-// const textVariants = cva("", {
-//   variants: {
-//     size: {
-//       sm: "text-sm [&:not(:first-child)]:mt-3",
-//       md: "text-md [&:not(:first-child)]:mt-6",
-//       lg: "text-lg [&:not(:first-child)]:mt-9",
-//     },
-//   },
-//   defaultVariants: {
-//     size: "md",
-//   },
-// });
 
 export type TextProps = React.HTMLAttributes<HTMLParagraphElement>;
 
