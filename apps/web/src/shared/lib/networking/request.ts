f697fe8a2c1fd3924e6bafca92cb6fb1d4ba9750
Saul-Liang <liang.saul@gmail.com>
2024-11-13 15:37:39 +0000

    update request functions

---
+++ apps/web/src/shared/lib/networking/request.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { get, nextClient, tmdbClient } from "./httpClients";
+import { RequestConfig } from "@/shared/models/request-config";
+
+// Internal
+
+export const getInternalWithSchema =
+  <S extends z.ZodType<T>, T>(responseSchema: S) =>
+  ([url, config]: [url: string, config?: RequestConfig | undefined]) =>
+    get({ client: nextClient, url, responseSchema, config });
+
+// External
+
+export const getTmdb = <S extends z.ZodType<T>, T>({
+  url,
+  responseSchema,
+  config,
+}: {
+  url: string;
+  responseSchema: S;
+  config?: RequestConfig | undefined;
+}) => get({ client: tmdbClient, url, responseSchema, config });
