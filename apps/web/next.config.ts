0aa1a86c1a7abf2985586ede940e7cd55e32b326
Saul-Liang <liang.saul@gmail.com>
2024-11-13 15:40:07 +0000

    update next config to allow fetching images from external sources

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@darkbluetechnologies/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/w500/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
