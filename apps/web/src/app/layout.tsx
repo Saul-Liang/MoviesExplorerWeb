7346d24b12f1dda3c68e0a73794b75e983e5ef7c
Saul-Liang <liang.saul@gmail.com>
2024-11-13 22:10:09 +0000

    update layout component

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,16 +5,9 @@ import { Inter } from "next/font/google";
 import {
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from "@darkbluetechnologies/ui/sidebar";
-import { Separator } from "@darkbluetechnologies/ui/separator";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-} from "@darkbluetechnologies/ui/breadcrumb";
 import { AppSidebar } from "@/shared/components/app-sidebar";
+import { cn } from "@ui/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,22 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className)}>
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
-            <main>
-              <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <Breadcrumb>
-                  <BreadcrumbList>
-                    <BreadcrumbItem>
-                      <BreadcrumbPage>Popular movies</BreadcrumbPage>
-                    </BreadcrumbItem>
-                  </BreadcrumbList>
-                </Breadcrumb>
-              </header>
+            <main className={cn("h-screen w-full overflow-auto")}>
               {children}
             </main>
           </SidebarInset>
