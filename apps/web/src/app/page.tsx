6375682036840fb39295c5687a429468a99c1b39
Saul-Liang <liang.saul@gmail.com>
2024-11-13 15:38:53 +0000

    update home page component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-import { usePopularMovies } from "@/shared/hooks/use-popular-movies";
-import { Heading } from "@darkbluetechnologies/ui/heading";
-import { useState } from "react";
+import { PopularMovies } from "@/components/home/popular-movies";
 
 export default function Home() {
-  const [page, setPage] = useState(1);
-  const { data, isLoading, error } = usePopularMovies({ page });
-
-  console.log(data);
-
   return (
     <div className="container">
-      <Heading variant="h1">Popular movies</Heading>
+      <PopularMovies />
     </div>
   );
 }
