c363aec5a13f401e3521fa2837f06775f3051264
Saul-Liang <liang.saul@gmail.com>
2024-11-13 22:09:20 +0000

    update popular-movies component

--- apps/web/src/components/home/popular-movies.tsx
+++ apps/web/src/components/home/popular-movies.tsx
@@ -1,95 +1,89 @@
 "use client";
 
-import { Heading } from "@ui/components/ui/heading";
-import { Text } from "@ui/components/ui/text";
-import Image from "next/image";
 import { usePopularMovies } from "@/shared/hooks/use-popular-movies";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Movie } from "@/shared/models/movie";
-import { isDefined } from "remeda";
-import { cn } from "@ui/lib/utils";
-import { format, parse, parseISO } from "date-fns";
+import { isDefined, isEmpty } from "remeda";
+import { MovieCard } from "./movie-card";
+import { LoadingSpinner } from "@/shared/components/loading-spinner";
+
+interface PopularMoviesState {
+  page: number;
+  totalPages: number;
+  latestPage: number;
+  movies: Movie[];
+}
 
 export function PopularMovies() {
-  const [page, setPage] = useState(1);
-  const [movies, setMovies] = useState<Movie[]>([]);
+  const [{ page, totalPages, latestPage, movies }, setPopularMoviesState] =
+    useState<PopularMoviesState>({
+      page: 1,
+      totalPages: 0,
+      latestPage: 0,
+      movies: [],
+    });
   const { data, isLoading, error } = usePopularMovies({ page });
 
+  function handleScroll(event: React.UIEvent<HTMLDivElement, UIEvent>) {
+    const bottom =
+      event.currentTarget.scrollHeight - event.currentTarget.scrollTop ===
+      event.currentTarget.clientHeight;
+    if (bottom && !isLoading) {
+      setPopularMoviesState((currentState) => ({
+        ...currentState,
+        page: currentState.page + 1,
+      }));
+    }
+  }
+
   useEffect(() => {
-    if (isDefined(data)) {
-      console.log("__DEBUG: setting movies");
-      setMovies((currentMovies) => [...currentMovies, ...data.results]);
+    if (isDefined(data) && data.page > latestPage) {
+      setPopularMoviesState((currentState) => {
+        const sanitisedMovies = data.results.filter(
+          (newMovie) =>
+            !currentState.movies.some((movie) => movie.id === newMovie.id),
+        );
+        const updatedMovies = [...currentState.movies, ...sanitisedMovies];
+        return {
+          page: currentState.page,
+          totalPages: data.total_results,
+          latestPage: data.page,
+          movies: updatedMovies,
+        };
+      });
     }
   }, [data]);
 
-  if (isLoading) {
-    return <div>loading...</div>;
+  if (isLoading && isEmpty(movies)) {
+    return (
+      <div className="flex w-full justify-center py-4">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (isDefined(error) && isEmpty(movies)) {
+    return (
+      <div className="flex w-full justify-center py-4">
+        Error loading popular movies
+      </div>
+    );
   }
 
   return (
-    <div className="grid grid-flow-row grid-cols-1 gap-4 py-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+    <div
+      className="grid max-h-full w-full grid-flow-row grid-cols-1 gap-4 overflow-y-scroll p-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4"
+      onScroll={handleScroll}
+    >
       {movies.map((movie) => (
-        <div
-          key={movie.id}
-          className={cn(
-            "flex max-h-48 min-h-48 overflow-hidden rounded-xl shadow-lg hover:shadow-xl",
-          )}
-        >
-          <div className={cn("relative w-24 min-w-24 bg-slate-400")}>
-            <Image
-              src={`${process.env.NEXT_PUBLIC_TMDB_POSTER_URL}${movie.poster_path}`}
-              alt={`Movie poster for ${movie.title}`}
-              fill={true}
-              className="object-cover"
-            />
-          </div>
-          <div className="flex flex-col p-4">
-            <Heading
-              variant="h4"
-              className={cn("line-clamp-1 text-xl text-slate-800")}
-            >
-              {movie.title}
-            </Heading>
-            <Text className={cn("line-clamp-1 text-sm text-slate-600")}>
-              {parseAndFormat(movie.release_date)}
-            </Text>
-            <Text
-              className={cn(
-                "mt-4 max-h-16 overflow-hidden text-ellipsis text-wrap text-sm text-slate-600",
-              )}
-            >
-              {movie.overview}
-            </Text>
-            <Text
-              className={cn(
-                "mt-4 line-clamp-5 text-right text-sm font-semibold text-slate-600",
-                getVoteColor(movie.vote_average),
-              )}
-            >
-              {getVoteAsPercentage(movie.vote_average)}
-            </Text>
-          </div>
-        </div>
+        <MovieCard key={movie.id} movie={movie} />
       ))}
+
+      {!isEmpty(movies) && page !== totalPages && (
+        <div className="col-span-full flex w-full justify-center py-4">
+          <LoadingSpinner />
+        </div>
+      )}
     </div>
   );
-}
-
-function parseAndFormat(dateString: string): string {
-  const date = parseISO(dateString);
-  return format(date, "dd MMM yyyy");
-}
-
-function getVoteAsPercentage(vote_average: number): string {
-  return Math.round(vote_average * 10).toString() + "%";
-}
-
-function getVoteColor(vote_average: number): string {
-  if (vote_average > 7) {
-    return "text-green-600";
-  } else if (vote_average > 4) {
-    return "text-orange-600";
-  } else {
-    return "text-red-600";
-  }
 }
