281aea3acf0f397b3faf67a79d602e5aa4cddc5d
Saul-Liang <liang.saul@gmail.com>
2024-11-15 00:06:43 +0000

    update home page component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,16 @@
-import { Home } from "@/components/home/home";
+import { PopularMovies } from "@/components/home/popular-movies";
+import { PageLayout } from "@/components/layout/page-layout";
+import { Breadcrumbs } from "@/shared/components/breadcrumbs";
+import { type Breadcrumb } from "@/shared/models/breadcrumb";
+
+const HOME_BREADCRUMBS: Breadcrumb[] = [
+  { id: "popular-movies", title: "Popular movies", url: null },
+];
 
 export default function HomePage() {
-  return <Home />;
+  return (
+    <PageLayout leftItem={<Breadcrumbs breadcrumbs={HOME_BREADCRUMBS} />}>
+      <PopularMovies />
+    </PageLayout>
+  );
 }
