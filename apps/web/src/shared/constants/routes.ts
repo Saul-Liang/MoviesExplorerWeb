9269b7e3a438511e4fc0009c3a98f7692650c8ea
Saul-Liang <liang.saul@gmail.com>
2024-11-23 15:09:13 +0000

    update routes constants

--- apps/web/src/shared/constants/routes.ts
+++ apps/web/src/shared/constants/routes.ts
@@ -1,16 +1,33 @@
 export const ROUTES = {
   PAGE: {
-    HOME: "/",
-    FAVOURITE_MOVIES: "/favourite-movies",
-  },
-  API: {
     INTERNAL: {
-      POPULAR_MOVIES: "/api/popular-movies",
+      HOME: "",
+      FAVOURITE_MOVIES: "favourite-movies",
     },
     EXTERNAL: {
       TMDB: {
-        POPULAR_MOVIES: "/3/movie/popular",
-        CREATE_REQUEST_TOKEN: "/3/authentication/token/new",
+        AUTHENTICATE: ({
+          request_token,
+          redirect_to,
+        }: {
+          request_token: string;
+          redirect_to: string;
+        }) => `authenticate/${request_token}?redirect_to=${redirect_to}`,
+      },
+    },
+  },
+  API: {
+    INTERNAL: {
+      POPULAR_MOVIES: "api/popular-movies",
+      CREATE_REQUEST_TOKEN: "api/create-request-token",
+      CREATE_SESSION: "api/create-session",
+    },
+    EXTERNAL: {
+      TMDB: {
+        POPULAR_MOVIES: "3/movie/popular",
+        MOVIE: (id: string) => `3/movie/${id}`,
+        CREATE_REQUEST_TOKEN: "3/authentication/token/new",
+        CREATE_SESSION: "3/authentication/session/new",
       },
     },
   },
