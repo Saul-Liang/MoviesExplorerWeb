14ee0bae648977c6a87fd0f0fb9f5cd8b6e74e51
Saul-Liang <liang.saul@gmail.com>
2024-11-23 15:11:04 +0000

    update create-request-token api route

--- apps/web/src/app/api/create-request-token/route.ts
+++ apps/web/src/app/api/create-request-token/route.ts
@@ -1,14 +1,14 @@
 import { ROUTES } from "@/shared/constants/routes";
 import { getTmdb } from "@/shared/lib/networking/request";
 import { RequestError } from "@/shared/models/error/request-error";
-import { requestTokenSchema } from "@/shared/models/request-token";
+import { createRequestTokenResponseSchema } from "@/shared/models/create-request-token";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
     const response = await getTmdb({
       url: ROUTES.API.EXTERNAL.TMDB.CREATE_REQUEST_TOKEN,
-      responseSchema: requestTokenSchema,
+      responseSchema: createRequestTokenResponseSchema,
       config: {
         headers: {
           Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
