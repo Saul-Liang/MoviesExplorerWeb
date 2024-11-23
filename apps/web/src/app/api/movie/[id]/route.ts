77bd86f8d1048140a40fdd906f8c2be18e04a9d0
Saul-Liang <liang.saul@gmail.com>
2024-11-23 15:10:19 +0000

    update movie api route

--- apps/web/src/app/api/movie/[id]/route.ts
+++ apps/web/src/app/api/movie/[id]/route.ts
@@ -1,24 +1,30 @@
+import { ROUTES } from "@/shared/constants/routes";
 import { getTmdb } from "@/shared/lib/networking/request";
 import { RequestError } from "@/shared/models/error/request-error";
 import { movieDetailsSchema } from "@/shared/models/movie-details";
 import { type NextRequest, NextResponse } from "next/server";
+import { isNonNull } from "remeda";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const movie_id = searchParams.get("id");
 
   try {
-    const response = await getTmdb({
-      url: `/3/movie/${movie_id}`,
-      responseSchema: movieDetailsSchema,
-      config: {
-        headers: {
-          Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+    if (isNonNull(movie_id)) {
+      const response = await getTmdb({
+        url: ROUTES.API.EXTERNAL.TMDB.MOVIE(movie_id),
+        responseSchema: movieDetailsSchema,
+        config: {
+          headers: {
+            Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+          },
         },
-      },
-    });
+      });
 
-    return NextResponse.json(response);
+      return NextResponse.json(response);
+    } else {
+      throw new RequestError({ message: "Movie ID is required", status: 400 });
+    }
   } catch (error) {
     if (error instanceof RequestError) {
       return new NextResponse(error.message, {
