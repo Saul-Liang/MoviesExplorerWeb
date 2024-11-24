c3a34fc3dab3fc7d46f1648cf503d9d376d54a9d
Saul-Liang <liang.saul@gmail.com>
2024-11-24 11:07:03 +0000

    fix sidebar issue

---
+++ packages/ui/src/components/ui/visually-hidden.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as RadixVisuallyHidden from "@radix-ui/react-visually-hidden";
+
+const VisuallyHidden = () => <RadixVisuallyHidden.Root />;
+
+export { VisuallyHidden };
